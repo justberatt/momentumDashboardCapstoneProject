8ff30841aae521e6dc13b5114448e81fbef3d6e6
Berat Sakipi <berat.sakipi@gmail.com>
2024-12-17 17:02:37 +0100

    Added the .catch() for error handling in case the fetch doesn't resolve. So I added a default picture

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+fetch("https://api.unsplash.com/photos/random?client_id=Y8fIJeQlFd-Oc0T5Bs4mrd0LI2D-js6xRXoyRCO6BRY&orientation=landscape&query=nature")
 	.then(res => res.json())
 	.then(data => {
-		const body = document.body
-		body.style.backgroundImage = `url(${data.urls.raw})`
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
-		console.log(data.urls.full)
 	})
+    .catch(err => {
+        document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1563216368-5b6a40648062?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MzQ0NDQ5ODd8&ixlib=rb-4.0.3&q=85")`
+		document.getElementById("author").textContent = `By: Hugues de BUYER-MIMEURE`
+    })
